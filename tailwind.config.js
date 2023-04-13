62a3cc16ffa69d42c942b8ac1b7afeba3b7f2bbc
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-13 06:32:08 +0430

    added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,23 @@ module.exports = {
 			fontFamily: {
 				iranyekan: ['iranyekan'],
 			},
+			fontSize: {
+				11: '11px',
+				14: '14px',
+				15: '15px',
+				16: '16px',
+			},
+			colors: {
+				primary: '#3880FF',
+				secondary300: '#D9DBE9',
+				secondary400: '#A0A3BD',
+				secondary600: '#4E4B66',
+				secondary800: '#14142A',
+			},
+			borderWidth: { 1: '1px', 1.5: '1.5px' },
+			boxShadow: {
+				calendar: '-4px 4px 16px rgba(0, 0, 0, 0.08)',
+			},
 		},
 	},
 	plugins: [],
