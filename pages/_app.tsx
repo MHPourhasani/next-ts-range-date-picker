391a5f30ac906d2b185b4165d2621b1c76f08ca5
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-15 02:36:21 +0430

    added styles file to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import '@/styles/datePicker.module.css'
+import '@/styles/calendar.module.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
