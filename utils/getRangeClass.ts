b236685499065ffff1d283d21d4f180bf158283a
Mohammad Hasan Pouhasani <mhpourhasani.dev@gmail.com>
2023-04-15 02:01:36 +0430

    created getRangeClass and getRangeHoverClass functions

---
+++ utils/getRangeClass.ts
@@ -0,0 +1,45 @@
+import isSameDate from './isSameDate';
+
+const getRangeClass = (
+	date: any,
+	selectedDate: any,
+	checkMonth: any,
+	oneDaySelectStyle: string,
+	rangeDateStyle: string,
+	startRangeDayStyle: string,
+	endRangeDayStyle: string
+) => {
+	let first = selectedDate[0],
+		second = selectedDate[1],
+		names = [];
+
+	if (selectedDate.length === 1) {
+		// if (isSameDate(date, first, checkMonth)) names.push('rmdp-range text-black bg-primary text-white rounded-md');
+		// if (isSameDate(date, first, checkMonth))
+		if (isSameDate(date, first)) {
+			names.push('bg-primary text-white rounded-xl');
+			// names.push(oneDaySelectStyle);
+		}
+	} else if (selectedDate.length === 2) {
+		if (date.toDays() >= first.toDays() && date.toDays() <= second.toDays()) {
+			names.push('bg-sky-200'); // rmdp-range
+		}
+
+		// if (isSameDate(date, first, checkMonth, startRangeDayStyle))
+		// if (isSameDate(date, first, checkMonth))
+		if (isSameDate(date, first)) {
+			// names.push('start');
+			names.push('bg-primary text-white rounded-r-xl');
+		}
+
+		// if (isSameDate(date, second, checkMonth))
+		if (isSameDate(date, second)) {
+			// names.push('end');
+			names.push('bg-primary text-white rounded-l-xl');
+		}
+	}
+
+	return names.join(' ');
+};
+
+export default getRangeClass;
---
+++ utils/getRangeHoverClass.ts
@@ -0,0 +1,31 @@
+const getRangeHoverClass = (
+	date: any,
+	selectedDate: any,
+	dateHovered: any,
+	rangeHover: any,
+	type: string = 'day'
+) => {
+	const names = [];
+
+	if (rangeHover && selectedDate?.length === 1 && dateHovered) {
+		const format = type === 'day' ? 'YYYY/MM/DD' : 'YYYY/MM';
+		const strHovered = dateHovered.format(format);
+		const strSelected = selectedDate[0].format(format);
+		const strDay = date.format(format);
+
+		if (
+			(strDay > strSelected && strDay <= strHovered) ||
+			(strDay < strSelected && strDay >= strHovered)
+		) {
+			names.push('bg-secondary300'); // rmdp-range-hover
+
+			if (strDay === strHovered) {
+				names.push(strHovered > strSelected ? 'end bg-black' : 'start bg-black rounded-xl');
+			}
+		}
+	}
+
+	return names;
+};
+
+export default getRangeHoverClass;
